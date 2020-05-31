8e4d8959ddc27febeda3695b3ac4c7278ed0969c
syukron alfarozi <40594982+sykrn@users.noreply.github.com>
2020-05-31 18:10:42 +0700

    Create autograder.py

---
+++ utils/autograder.py
@@ -0,0 +1,69 @@
+from unittest.mock import patch 
+from io import StringIO 
+
+# match variable output
+def var_cmp(v1,v2):
+  passed = 'Salah'
+  if v1 == v2:
+    passed = 'Benar'
+  l = [passed,v1,type(v1),v2,type(v2)]
+  return l
+
+def func_cmp(f1,f2,*args,**kwargs):
+  passed = 'Salah'
+  e=None
+  o1 = f1(*args,**kwargs)
+  try:
+    o2 = f2(*args,**kwargs)
+  except Exception as er:
+    e=er
+  finally:
+    if e:
+      return ['Error',str(e)]
+    else:
+      if o1 == o2:
+        passed = 'Benar'
+      l = [passed,o1,type(o1),o2,type(o2)]
+      return l
+
+def get_printout(f,*args,**kwargs):    
+    with patch('sys.stdout', new = StringIO()) as fake_out: 
+        f(*args,**kwargs)
+        return fake_out.getvalue()
+
+def funcout_cmp(f1,f2,*args,**kwargs):
+  passed = 'Salah'
+  e=None
+  o1 = get_printout(f1,*args,**kwargs)
+  try:
+    o2 = get_printout(f2,*args,**kwargs)
+  except Exception as er:
+    e=er
+  finally:
+    if e:
+      return ['Error',str(e)]
+    else:
+      if o1 == o2:
+        passed = 'Benar'
+      l = [passed,o1,type(o1),o2,type(o2)]
+      return l
+
+class CmpTestcase():
+  out = None
+  def __init__(self, v1,v2,tCases=None):
+    self.tCases=tCases
+    if not callable(v1):
+      self.out = var_cmp(v1,v2)
+    elif tCases==None:
+      raise 'Please provide tCases list of tupple [(*args,*kwargs)]'
+    elif len(tCases)>1:
+      self.out = []
+      for args,kwargs in tCases:
+       out = func_cmp(v1,v2,*args,**kwargs)
+       self.out.append(out)
+    else:
+      self.out = funcout_cmp(v1,v2,*args,**kwargs)
+
+  def __str__(self):
+    print(self.out,self.tCases)
+    
