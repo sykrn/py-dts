72036cc1a709320914aca5eddbac4fda1947ef82
syukron alfarozi <40594982+sykrn@users.noreply.github.com>
2020-06-05 06:21:54 +0700

    Create p1.py

---
+++ project/p1.py
@@ -0,0 +1,74 @@
+# nama file p1.py 
+# Isikan email anda dan copy semua cell code yang dengan komentar #Graded
+# Isikan juga priority file
+
+# untuk resubmisi, grader hanya akan mengambil priority yang paling besar
+# jadi kalau anda ingin merevisi kode anda:
+# silakan di resubmit dengan nilai variable priority yang lebih besar dari
+# nilai priority submisi sebelumnya
+
+priority = 0
+
+
+#netacad email cth: 'abcd@gmail.com'
+email='abcd@gmail.com'
+
+# copy-paste semua #Graded cells di bawah ini
+
+# PASTE KODE ANDA DISINI 
+#Graded
+
+# Manual, filter, list comprehension
+def letter_catalog(items,letter='A'):
+  pass
+  # MULAI KODEMU DI SINI
+  # ini kode saya dst TENTUNYA SUDAH DIKERJAKAN!
+
+#Graded
+from collections import Counter
+
+# pakai library 
+def counter_item(items):
+  pass
+  # MULAI KODEMU DI SINI
+  # ini kode saya dst TENTUNYA SUDAH DIKERJAKAN!
+
+# tanpa library
+def counter_item2(items):
+  pass
+  # MULAI KODEMU DI SINI
+  # ini kode saya dst TENTUNYA SUDAH DIKERJAKAN!
+
+
+#Graded
+
+# dua variable berikut jangan diubah
+fruits = ['Apple','Avocado','Banana','Blackberries','Blueberries','Cherries','Date Fruit','Grapes','Guava','Jackfruit','Kiwifruit']
+prices = [6,5,3,10,12,7,14,15,8,7,9]
+
+# list buah
+chart = ['Blueberries','Blueberries','Grapes','Apple','Apple','Apple','Blueberries','Guava','Jackfruit','Blueberries','Jackfruit']
+chart.sort()
+
+# MULAI KODEMU DI SINI
+fruit_price = None # ini kode saya dst TENTUNYA SUDAH DIKERJAKAN!
+
+def total_price(dcounter,fprice):
+  pass
+  # MULAI KODEMU DI SINI
+  # ini kode saya dst
+
+#Graded
+
+def discounted_price(total,discount,minprice=100):
+  pass
+  # MULAI KODEMU DI SINI
+  # ini kode saya dst TENTUNYA SUDAH DIKERJAKAN!
+
+
+#Graded
+
+def print_summary(items,fprice):
+  pass
+  # MULAI KODEMU DI SINI
+  # ini kode saya dst TENTUNYA SUDAH DIKERJAKAN!
